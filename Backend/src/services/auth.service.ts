f3509b8363f31e331574b545ab53c81633e23cda
ndwiga <brayohndwiga@gmail.com>
2025-01-17 10:53:46 +0300

    initial commit

---
+++ Backend/src/services/auth.service.ts
@@ -0,0 +1,48 @@
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
+
+import { login_details } from '../interfaces/user';
+
+export class authService{
+
+    async login(logins: login_details){
+        let pool = await mssql.connect(sqlConfig)
+
+        let user = (await pool.request()
+        .input("email", logins.email)
+        .input("password", logins.password).execute("loginUser")).recordset
+
+        if(user.length < 1){
+            return {
+                error: "user not found"
+            }
+        }else{
+            
+            let hashedPassword = user[0].password
+
+            //compare password
+            let passwordMatches = bcrypt.compareSync(logins.password, hashedPassword)
+
+            if(passwordMatches){
+                let {createdAt, password, phone_number, ...rest } = user[0]
+
+                let token = jwt.sign(rest, process.env.SECRET_KEY as string, {
+                    expiresIn: '2h'
+                })
+
+                return {
+                    message: "Logged in successfully",
+                    token
+                }
+                
+            }else{
+                return {
+                    error: "Incorrect password"
+                }
+            }
+           
+        }
+    }
+}
